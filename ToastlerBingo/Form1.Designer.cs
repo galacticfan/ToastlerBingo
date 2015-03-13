15b37775b96e6efccaf42ec36341191cfe196697
galacticfan <galacticfan@server.fake>
2015-03-13 18:44:29 +0000

    Initial code

---
+++ ToastlerBingo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ToastlerBingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // printBtn
+            // 
+            this.printBtn.Location = new System.Drawing.Point(12, 12);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(102, 71);
+            this.printBtn.TabIndex = 0;
+            this.printBtn.Text = "Print Random Bingo Sheet";
+            this.printBtn.UseVisualStyleBackColor = true;
+            this.printBtn.Click += new System.EventHandler(this.printBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(131, 98);
+            this.Controls.Add(this.printBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button printBtn;
+    }
+}
+
---
+++ ToastlerBingo/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Novacode;
+
+namespace ToastlerBingo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void printBtn_Click(object sender, EventArgs e)
+        {
+            using (DocX document = DocX.Create("tableToPrint.docx")) // create doc
+            {
+                Table table = document.AddTable(3, 3);
+                table.Alignment = Alignment.center;
+
+                // Add content to table
+                table.Rows[0].Cells[0].Paragraphs.First().Append("A");
+                table.Rows[0].Cells[1].Paragraphs.First().Append("B");
+                table.Rows[0].Cells[2].Paragraphs.First().Append("C");
+                table.Rows[1].Cells[0].Paragraphs.First().Append("D");
+                table.Rows[1].Cells[1].Paragraphs.First().Append("E");
+                table.Rows[1].Cells[2].Paragraphs.First().Append("F");
+                table.Rows[2].Cells[0].Paragraphs.First().Append("D");
+                table.Rows[2].Cells[1].Paragraphs.First().Append("E");
+                table.Rows[2].Cells[2].Paragraphs.First().Append("F");
+
+                document.InsertTable(table);
+                document.Save();
+            }
+        }
+
+        private string generateRandom()
+        {
+
+        }
+    }
+}
