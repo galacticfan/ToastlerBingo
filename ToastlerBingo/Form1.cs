9a685e6730a70221efac73b0555f1f68bdfc27f6
galacticfan <galacticfan@server.fake>
2015-03-13 19:38:06 +0000

    Further refinements

--- ToastlerBingo/Form1.cs
+++ ToastlerBingo/Form1.cs
@@ -38,12 +38,186 @@ namespace ToastlerBingo
 
                 document.InsertTable(table);
                 document.Save();
+
+                // temp
+                try
+                {
+                    string[] listOfStrings = generateRandom();
+                    string list = String.Empty;
+                    for (int i = 0; i < listOfStrings.Length; i++)
+                    {
+                        list += listOfStrings[i] + Environment.NewLine;
+                    }
+                    MessageBox.Show(list);    
+                    //MessageBox.Show(listOfStrings[0]);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
             }
         }
 
-        private string generateRandom()
+        private string[] generateRandom()
         {
+            // Reference strings
+            string one = "\"This is for your notes\"";
+            string two = "\"Don\'t write this down\"";
+            string three = "\"Does everyone agree?\"";
+            string four = "Hands out hand-outs really quickly";
+            string five = "Starts writing at the top of the board";
+            string six = "\"File paper please\"";
+            string seven = "Starts talking about son";
+            string eight = "Mentions her bad memory";
+            string nine = "Finishes lessons 5 minutes late";
+            string ten = "Forgets names";
+            string eleven = "Uses colour rings";
+            string twelve = "Uses the ruler on the board";
+            string thirteen = "Talks to Randles";
+            string fourteen = "Gives you hand out as you walk in the lesson";
 
+            Random rnd = new Random();
+            int rndNum = 0;
+            int counter = 0;
+
+            string[] currentList = new string[9];
+
+            while (arrayCounter(currentList) < 9)
+            {
+                rndNum = rnd.Next(1, 15);
+                switch(rndNum)
+                {
+                    case 1:
+                        if (alreadyUsed(currentList, one) == false)
+                        {
+                            currentList[counter] = one;
+                            counter += 1;
+                        }
+                        break;
+                    case 2:
+                        if (alreadyUsed(currentList, two) == false)
+                        {
+                            currentList[counter] = two;
+                            counter += 1;
+                        }
+                        break;
+                    case 3:
+                        if (alreadyUsed(currentList, three) == false)
+                        {
+                            currentList[counter] = three;
+                            counter += 1;
+                        }
+                        break;
+                    case 4:
+                        if (alreadyUsed(currentList, four) == false)
+                        {
+                            currentList[counter] = four;
+                            counter += 1;
+                        }
+                        break;
+                    case 5:
+                        if (alreadyUsed(currentList, five) == false)
+                        {
+                            currentList[counter] = five;
+                            counter += 1;
+                        }
+                        break;
+                    case 6:
+                        if (alreadyUsed(currentList, six) == false)
+                        {
+                            currentList[counter] = six;
+                            counter += 1;
+                        }
+                        break;
+                    case 7:
+                        if (alreadyUsed(currentList, seven) == false)
+                        {
+                            currentList[counter] = seven;
+                            counter += 1;
+                        }
+                        break;
+                    case 8:
+                        if (alreadyUsed(currentList, eight) == false)
+                        {
+                            currentList[counter] = eight;
+                            counter += 1;
+                        }
+                        break;
+                    case 9:
+                        if (alreadyUsed(currentList, nine) == false)
+                        {
+                            currentList[counter] = nine;
+                            counter += 1;
+                        }
+                        break;
+                    case 10:
+                        if (alreadyUsed(currentList, ten) == false)
+                        {
+                            currentList[counter] = ten;
+                            counter += 1;
+                        }
+                        break;
+                    case 11:
+                        if (alreadyUsed(currentList, eleven) == false)
+                        {
+                            currentList[counter] = eleven;
+                            counter += 1;
+                        }
+                        break;
+                    case 12:
+                        if (alreadyUsed(currentList, twelve) == false)
+                        {
+                            currentList[counter] = twelve;
+                            counter += 1;
+                        }
+                        break;
+                    case 13:
+                        if (alreadyUsed(currentList, thirteen) == false)
+                        {
+                            currentList[counter] = thirteen;
+                            counter += 1;
+                        }
+                        break;
+                    case 14:
+                        if (alreadyUsed(currentList, fourteen) == false)
+                        {
+                            currentList[counter] = fourteen;
+                            counter += 1;
+                        }
+                        break;
+                }
+            }
+
+            return currentList;
+        }
+
+        private bool alreadyUsed(string[] inputArray, string phrase)
+        {
+            bool hasBeenUsed = false;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == phrase)
+                {
+                    hasBeenUsed = true;
+                    break;
+                }
+            }
+
+            return hasBeenUsed;
+        }
+
+        private int arrayCounter(string[] inputArray)
+        {
+            int length = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == null)
+                    break;
+                else
+                    length += 1;
+            }
+            return length;
         }
     }
 }
